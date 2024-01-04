37c3b179b92aa28adc40f09bacc08e5576fa6f68
morozgit <morozec76@gmail.com>
2024-01-04 22:45:13 +0300

    now bot starts

--- main.py
+++ main.py
@@ -19,22 +19,27 @@ city = City()
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    city.SetName('Привет) Введи название города.')
-    bot.send_message(message.chat.id, text=city.GetName())
-    
+    bot.send_message(message.chat.id, text='Привет) Первый город Москва.')
+    city.SetName('Москва')
+    city.UpdateCityExist('Москва')
+
 
 @bot.message_handler(func=lambda message: True)
 def discuss_with_bot(message):
-    is_city_exist = city.CheckCityExist(message.text)
-    city.UpdateCityExist(message.text)
-    print(is_city_exist)
-    last_letter = message.text[-1]
-    city_name = city.FindCity(last_letter.upper())
-    print(is_city_exist)
-    if city_name and (is_city_exist == 0):
-        bot.send_message(message.chat.id, text=city_name)
-        city.UpdateCityExist(city_name)
+    city_name = city.GetName()
+    if message.text[0] == city_name[-1].upper():
+        is_city_exist = city.CheckCityExist(message.text)
+        city.UpdateCityExist(message.text)
+        print(message.text[0], city_name[-1])
+        if is_city_exist == 0:
+            last_letter = message.text[-1]
+            found_city_name = city.FindCity(last_letter.upper())
+            bot.send_message(message.chat.id, text=found_city_name)
+            city.SetName(found_city_name)
+            city.UpdateCityExist(found_city_name)
+        else:
+            bot.send_message(message.chat.id, text='Уже было')
     else:
-        bot.send_message(message.chat.id, text="Город был назван")
+        bot.send_message(message.chat.id, text='Надо на последнюю букву')
 
 bot.infinity_polling()
